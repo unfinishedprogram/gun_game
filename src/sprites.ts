5e270c0f4971e9d24628117585c63a7a46444785
Noah Labrecque <noah.labrecque@gmail.com>
2021-05-22 11:32:51 -0400

    made sprite loading cleaner

--- src/sprites.ts
+++ src/sprites.ts
@@ -8,21 +8,28 @@ export abstract class Sprites{
     static gun:ISprite;
     static bullet:ISprite;
     static muzzleflash:ISprite;
+
     static initalize(){
         this.gun = {
-            image: document.getElementById("gun") as HTMLImageElement,
+            image: this.fromId("gun"),
             width: 100,
             height: 64,
         }
+
         this.bullet = {
-            image: document.getElementById("bullet") as HTMLImageElement,
+            image: this.fromId("bullet"),
             width: 30,
             height: 15,
         }
+
         this.muzzleflash = {
-            image: document.getElementById("muzzleflash") as HTMLImageElement,
+            image: this.fromId("muzzleflash"),
             width: 150,
             height: 75,
         }
+    }
+    
+    static fromId(id:string){ 
+        return document.getElementById(id) as HTMLImageElement
     }
 }
